c83b90f64394d72527148f80596df2135f156e9e
coder-WJie <2623312985@qq.com>
2021-03-01 20:34:05 +0800

    购物车navBar

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-
 import mutations from './mutations'
 import actions from './actions'
+import getters from './getters'
 
 //1.安装插件
 Vue.use(Vuex)
@@ -16,7 +16,7 @@ const store = new Vuex.Store({
     state,
     mutations,
     actions,
-
+    getters,
 })
 
 //3.挂载到Vue实例上
