93facec229b0bd56eff22ad1acb86cc1e8be7034
coder-WJie <2623312985@qq.com>
2021-03-02 20:00:28 +0800

    CartListItem组件封装

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -3,24 +3,39 @@
     <nav-bar class="navBar"
       ><div slot="center">购物车({{ cartLength }})</div></nav-bar
     >
+    <cart-list></cart-list>
   </div>
 </template>
 <script>
 import BScroll from "better-scroll";
 import NavBar from "../../components/common/navbar/NavBar.vue";
 
+// import { mapGetters } from "vuex";
+import CartList from "./childComps/cartList.vue";
+
 export default {
   name: "Category",
-  components: { NavBar },
+  components: { NavBar, CartList },
   computed: {
     cartLength() {
-      // return this.$store.state.cartList.length;
       return this.$store.getters.cartLength;
     },
+    //两种语法
+    /* 1.
+    ...mapGetters(['cartLength','cartList']) 数组中一一对应
+       2.
+    ...mapGetters({
+      length:'cartLength',
+      list:'cartList'
+    })   
+     */
   },
 };
 </script>
 <style scoped>
+.cart {
+  height: 100vh;
+}
 .navBar {
   background-color: var(--color-tint);
   color: #fff;
---
+++ src/views/category/childComps/cartList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="cart-list">
+    <scroll class="content" ref="scroll">
+      <cart-list-item
+        v-for="(item, index) in cartList"
+        :key="index"
+        :product="item"
+      />
+    </scroll>
+  </div>
+</template>
+
+<script>
+import Scroll from "@/components/common/scroll/Scroll";
+import { mapGetters } from "vuex";
+import CartListItem from "./CartListItem.vue";
+
+export default {
+  components: { CartListItem, Scroll },
+  name: "CartList",
+  computed: {
+    ...mapGetters(["cartList"]),
+  },
+  activated() {
+    this.$refs.scroll.refresh();
+  },
+};
+</script>
+<style scoped>
+.cart-list {
+  height: calc(100% - 44px - 49px);
+}
+.content {
+  height: 100%;
+  overflow: hidden;
+}
+</style>
